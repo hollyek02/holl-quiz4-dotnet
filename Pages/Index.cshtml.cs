0d5051cdc6e4685a7301f0dabc8a84c87defcc48
Holly Edwards-Kiss <hedwar15@my.centennialcollege.ca>
2026-04-09 10:35:41 -0400

    Add database access message

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -9,6 +9,7 @@ public class IndexModel : PageModel
     public required string DbMessage { get; set; }
     public void OnGet()
     {
-        DbMessage = "Database access has already been done on April 6, 2026 by Holly Edwards-Kiss";
+        // Simulated database access completed for quiz 4
+        DbMessage = "Database access has already been done on April 6, 2026 by Holly Edwards-Kiss.";
     }
 }
